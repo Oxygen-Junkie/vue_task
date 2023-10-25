92fc1f17d46b8d433097b3e80f2c9b06ae4b4ef1
John Doe <johndoe@email.com>
2023-10-25 18:28:58 +0400

    .

--- src/components/createReply_modal.vue
+++ src/components/createReply_modal.vue
@@ -20,32 +20,30 @@ function postReply() {
 
 <template>
     <teleport to="body">
-      <div class="modal-overlay" @click.prevent="() => { emit('close') }">
-        <div class="fixed center z-999">
-          <transition name="modal">
-                <div>
-                    <div class="bg-green-500 rounded-t-md">
-                        <div class="text-right mr-2">
-                        <button title="Закрыть окно" class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" @click.prevent="() => { emit('close') }">
-                            <strong class="text-xl align-center cursor-pointer alert-del">&times;</strong>
-                        </button>
-                        </div>
-                    </div>
-                    <div class="card-container rounded-b-md bg-gray-200 dark:bg-gray-500">
-                        <div v-if="error" class="relative text-red-300 px-3 py-3 mb-4 border rounded text-red-darker border-red-dark bg-red-lighter" >
-                            Вы не указали текст отзыва
-                        </div>
-                        <textarea
-                          v-model.trim="reply.comment"
-                          class="block appearance-none w-full h-50 py-1 px-2 mb-1 text-base leading-normal bg-white text-grey-darker border border-grey rounded"
-                          placeholder="Введите текст отзыва"
-                        />
-                        <button title="Разместить отзыв" class="inline-flex text-center text-white p-1 font-semibold text-sm align-baseline leading-none rounded bg-green-500" @click="postReply()">Разместить отзыв</button>
-                    </div>
-                </div>
-            </transition>
-          </div>
-        </div>
+      <div class="fixed center z-999">
+        <transition name="modal">
+              <div>
+                  <div class="bg-green-500 rounded-t-md">
+                      <div class="text-right mr-2">
+                      <button title="Закрыть окно" class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" @click.prevent="() => { emit('close') }">
+                          <strong class="text-xl align-center cursor-pointer alert-del">&times;</strong>
+                      </button>
+                      </div>
+                  </div>
+                  <div class="card-container rounded-b-md bg-gray-200 dark:bg-gray-500">
+                      <div v-if="error" class="relative text-red-300 px-3 py-3 mb-4 border rounded text-red-darker border-red-dark bg-red-lighter" >
+                          Вы не указали текст отзыва
+                      </div>
+                      <textarea
+                        v-model.trim="reply.comment"
+                        class="block appearance-none w-full h-50 py-1 px-2 mb-1 text-base leading-normal bg-white text-grey-darker border border-grey rounded"
+                        placeholder="Введите текст отзыва"
+                      />
+                      <button title="Разместить отзыв" class="inline-flex text-center text-white p-1 font-semibold text-sm align-baseline leading-none rounded bg-green-500" @click="postReply()">Разместить отзыв</button>
+                  </div>
+              </div>
+          </transition>
+      </div>
     </teleport>
 </template>
 
